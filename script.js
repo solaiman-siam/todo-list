afa1ede803c2bf353755c3e10cb12e135db76c27
solaiman_siam <solaimansiam93@gmail.com>
2024-02-17 14:03:18 +0600

    add some validation

--- script.js
+++ script.js
@@ -5,24 +5,30 @@ let ul = document.getElementById("ul");
 function clickBtn() {
   let inputVal = input.value;
 
-  let li = document.createElement("li");
-  li.classList.add(
-    "flex",
-    "justify-between",
-    "items-center",
-    "space-y-3",
-    "li"
-  );
-  li.innerHTML = `  <div class="flex items-center h-6">
-    <div id='circle' class="w-5 h-5 pb-2 rounded-full border-2 border-[#e0e0e0] mr-3 circle"></div>
-    <img id='check'  class="w-5 h-5 mr-3  hidden check" src="images/check.png" alt="">
-    <p id='texts'  class="text-sm leading-none h-fit font-medium">${inputVal}
-    <p>
-</div>
-<img id='image' class="w-5 h-5 px-[5px] rounded-full hover:bg-[#bebebe] py-[5px] bg-[#e0e0e0]" src="images/close.png" alt="">
-`;
-  ul.appendChild(li);
-  input.value = "";
+  if(input.value === ''){
+    alert('Please add your task');
+  } else{
+    let li = document.createElement("li");
+    li.classList.add(
+      "flex",
+      "justify-between",
+      "items-center",
+      "space-y-3",
+      "li"
+    );
+    li.innerHTML = `  <div class="flex items-center h-6">
+      <div id='circle' class="w-5 h-5 pb-2 rounded-full border-2 border-[#e0e0e0] mr-3 circle"></div>
+      <img id='check'  class="w-5 h-5 mr-3  hidden check" src="images/check.png" alt="">
+      <p id='texts'  class="text-sm leading-none h-fit font-medium">${inputVal}
+      <p>
+  </div>
+  <img id='image' class="w-5 h-5 px-[5px] rounded-full hover:bg-[#bebebe] py-[5px] bg-[#e0e0e0]" src="images/close.png" alt="">
+  `;
+    ul.appendChild(li);
+    input.value = "";
+  }
+
+  
 }
 
 ul.addEventListener("click", function (event) {
